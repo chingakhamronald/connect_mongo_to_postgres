e1e4b087ab92f7052d868a8f7d44c31653d8bf71
Cr7 <cr7@pop-os.localdomain>
2022-12-30 10:35:26 +0530

    pagination bug fix

--- app/monthly/controller.js
+++ app/monthly/controller.js
@@ -2,21 +2,24 @@ const prisma = require("../../constant/client");
 
 module.exports.Monthly = async (req, res) => {
   const take = Number(req.query.pageSize);
-
   const pageNo = req.query.pageNo;
-
   const skip = Math.max(pageNo - 1, 0) * take;
-
   try {
     const monthly = await prisma.card.findMany({
       take: take,
       skip: skip,
     });
-
-    const updateData = JSON.stringify(monthly, (_, v) =>
-      typeof v === "bigint" ? v.toString() : v
+    const totalMonthlyData = await prisma.card.count();
+    const updateData = JSON.parse(
+      JSON.stringify(monthly, (_, v) => {
+        if (typeof v === "bigint") console.log({ v });
+        return typeof v === "bigint" ? v.toString() : v;
+      })
     );
-    res.status(200).json(updateData);
+
+    res
+      .status(200)
+      .json({ monthlyData: updateData, totalMonthlyData: totalMonthlyData });
   } catch (err) {
     res.status(500).json(err.message);
   }
